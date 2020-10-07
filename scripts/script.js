f1a8eb6fc2a4908abb1347c1243a0191b647f06d
PommedeR <70270544+PommedeR@users.noreply.github.com>
2020-10-07 13:53:14 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+console.log('script');
+
+var menuButton = document.querySelector ('#menu');
+
+var menuLijst = document.querySelector ('menu1');
+
+menuButton.onclick = function(){
+document.getElementById('menu').classList.toggle('menu')
+}
+
+menuButton.addEventListener('click', menuLijst);
+
+console.log ('hoi')
